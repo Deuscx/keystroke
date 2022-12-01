3fe1005a41910a9485000a02a693af1efa5c34c4
deuscx <1475453714@qq.com>
2022-12-01 22:16:39 +0800

    feat: add rdev to support listen

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -9,9 +9,29 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+use rdev::{listen, Event};
+
+
 fn main() {
+    println!("start listening on");
+
+            // This will block.
+            if let Err(error) = listen(callback) {
+                println!("Error: {:?}", error)
+            };
+        
+            fn callback(event: Event) {
+                println!("My callback {:?}", event);
+                match event.name {
+                    Some(string) => println!("User wrote {:?}", string),
+                    None => (),
+                }
+            }      
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+    
+    
+
 }
