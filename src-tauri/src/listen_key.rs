c7380e5a7e78b1a31bf3edcd35d24d503a1404f8
deuscx <1475453714@qq.com>
2022-12-10 22:12:49 +0800

    feat: use if let

--- src-tauri/src/listen_key.rs
+++ src-tauri/src/listen_key.rs
@@ -7,16 +7,23 @@ pub fn create_device_query_listener(handle: tauri::AppHandle) {
 
     std::thread::spawn(move || {
         let callback = move |event: Event| {
-            match event.event_type {
-                EventType::KeyRelease(key) => {
-                    handle.emit_to("main", "KeyRelease",  utils::get_key(key) );
-                }
-                EventType::KeyPress(key) => {
-                    println!("{:?}", key);
-                    // handle.emit_to("main", "keypress","");
-                }
-                _ => println!("event ignored")
+            if let EventType::KeyRelease(key ) = event.event_type {
+                handle.emit_to("main", "KeyRelease",  utils::get_key(key) );
             }
+
+            if let EventType::KeyPress(key) = event.event_type {
+                handle.emit_to("main", "KeyPress",  utils::get_key(key));
+            }
+            // match event.event_type {
+            //     EventType::KeyRelease(key) => {
+                   
+            //     }
+            //     EventType::KeyPress(key) => {
+            //         println!("{:?}", key);
+            //         // handle.emit_to("main", "keypress","");
+            //     }
+            //     _ => println!("event ignored")
+            // }
 
         };
 
